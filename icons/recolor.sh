7212ba2e8feffd820688ee0c9b9f94bb9b034eba
MNandor <mnandor@users.noreply.github.com>
2023-02-01 11:28:22 +0200

    Add recolor script

---
+++ icons/recolor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+white="stroke\s*:\s*\(#fff\|\#ffffff\|white\)"
+
+# If you want to replace all stroke colors (won't match rgb()/hsl()/..., but will match #... and black/blue/...):
+# ="stroke\s*:\s*#\?[[:alnum:]]\+"
+black="stroke:#FF0000" # can replace with any target color
+for x in white/*.svg; do
+	sed -i "s/${white}/${black}/Ig" "${x}"
+done
+
